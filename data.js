2dad2687d23b833aed126e36b65476ff7c030fe3
cjh-png <cjh@twscps.edu.hk>
2026-05-06 11:09:45 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,33 +1,69 @@
 // 自動生成，請勿手動修改
 const fileList = [
   {
-    "name": "Happy Music [Copyright Free Music] - Lights by ‪.mp3",
-    "path": "files/Happy Music [Copyright Free Music] - Lights by ‪.mp3",
-    "type": "audio",
-    "size": "6.00MB"
+    "name": "balloons.jpg",
+    "path": "files/balloons.jpg",
+    "type": "image",
+    "size": "0.60MB"
   },
   {
-    "name": "IMG_0152.mp4",
-    "path": "files/IMG_0152.mp4",
+    "name": "blowing candles.mov",
+    "path": "files/blowing candles.mov",
     "type": "video",
-    "size": "3.16MB"
+    "size": "21.02MB"
   },
   {
-    "name": "TWS02880.JPG",
-    "path": "files/TWS02880.JPG",
+    "name": "cake.jpg",
+    "path": "files/cake.jpg",
     "type": "image",
-    "size": "11.22MB"
+    "size": "0.52MB"
   },
   {
-    "name": "TWS02881.JPG",
-    "path": "files/TWS02881.JPG",
+    "name": "candy.jpg",
+    "path": "files/candy.jpg",
     "type": "image",
-    "size": "10.33MB"
+    "size": "0.53MB"
   },
   {
-    "name": "TWS02882.JPG",
-    "path": "files/TWS02882.JPG",
+    "name": "crown.jpg",
+    "path": "files/crown.jpg",
     "type": "image",
-    "size": "9.46MB"
+    "size": "0.29MB"
+  },
+  {
+    "name": "cupcakes.jpg",
+    "path": "files/cupcakes.jpg",
+    "type": "image",
+    "size": "0.44MB"
+  },
+  {
+    "name": "girl.jpg",
+    "path": "files/girl.jpg",
+    "type": "image",
+    "size": "0.46MB"
+  },
+  {
+    "name": "mixing fruit.mp4",
+    "path": "files/mixing fruit.mp4",
+    "type": "video",
+    "size": "6.54MB"
+  },
+  {
+    "name": "painting.mp4",
+    "path": "files/painting.mp4",
+    "type": "video",
+    "size": "5.69MB"
+  },
+  {
+    "name": "people talking.mp4",
+    "path": "files/people talking.mp4",
+    "type": "video",
+    "size": "6.97MB"
+  },
+  {
+    "name": "stage.mov",
+    "path": "files/stage.mov",
+    "type": "video",
+    "size": "48.46MB"
   }
 ];
